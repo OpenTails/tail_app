79d1e6eabc53de1f8cdf3ec28d5e9768cba95e79
Codel1417 <13484789+Codel1417@users.noreply.github.com>
2024-02-20 12:51:25 -0500

    Merge Volume Control lib into main

--- android/app/src/main/kotlin/com/codel1417/tail_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/codel1417/tail_app/MainActivity.kt
@@ -1,7 +1,22 @@
 package com.codel1417.tail_app
 
-import dev.darttools.flutter_android_volume_keydown.FlutterAndroidVolumeKeydownActivity;
+import android.view.KeyEvent
+import android.view.KeyEvent.KEYCODE_VOLUME_DOWN
+import android.view.KeyEvent.KEYCODE_VOLUME_UP
+import dev.darttools.flutter_android_volume_keydown.FlutterAndroidVolumeKeydownPlugin.eventSink
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity :
-    FlutterAndroidVolumeKeydownActivity() { //Hopefully this doesn't cause problems later
+
+class MainActivity : FlutterActivity() {
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KEYCODE_VOLUME_DOWN && eventSink != null) {
+            eventSink.success(true)
+            return true
+        }
+        if (keyCode == KEYCODE_VOLUME_UP && eventSink != null) {
+            eventSink.success(false)
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
 }
