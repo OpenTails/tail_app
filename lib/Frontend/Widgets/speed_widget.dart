373ae6c6fcda273f63c079a6a9a1fd1482edc7a6
Codel1417 <13484789+Codel1417@users.noreply.github.com>
2024-03-08 21:42:48 -0500

    Reverse speed slider

--- lib/Frontend/Widgets/speed_widget.dart
+++ lib/Frontend/Widgets/speed_widget.dart
@@ -11,14 +11,18 @@ class SpeedWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-        title: Text(sequencesEditSpeed()),
-        subtitle: Slider(
+      title: Text(sequencesEditSpeed()),
+      subtitle: Directionality(
+        textDirection: TextDirection.rtl,
+        child: Slider(
           label: "${(value.toInt() * 20).toInt()}ms",
           value: value,
           min: 15,
           max: 127,
           divisions: 110,
           onChanged: onChanged,
-        ));
+        ),
+      ),
+    );
   }
 }
