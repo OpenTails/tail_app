fba6d426f448e9923be32a07461fe81ec251eb3f
Codel1417 <13484789+Codel1417@users.noreply.github.com>
2024-03-08 21:37:27 -0500

    Fix analytics

--- lib/Backend/PlausibleDio.dart
+++ lib/Backend/PlausibleDio.dart
@@ -4,9 +4,11 @@ import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:plausible_analytics/plausible_analytics.dart';
 import 'package:sentry_hive/sentry_hive.dart';
+import 'package:tail_app/Backend/moveLists.dart';
 
 import '../main.dart';
 import 'Definitions/Device/BaseDeviceDefinition.dart';
+import 'Sensors.dart';
 
 class PlausibleDio extends Plausible {
   PlausibleDio(super.serverUrl, super.domain);
@@ -26,9 +28,10 @@ class PlausibleDio extends Plausible {
     }
     page = "app://localhost/$page";
     referrer = "app://localhost/$referrer";
+    props = Map.of(props);
     props['Number Of Devices'] = SentryHive.box<BaseStoredDevice>('devices').length.toString();
-    props['Number Of Sequences'] = SentryHive.box<BaseStoredDevice>('sequences').length.toString();
-    props['Number Of Triggers'] = SentryHive.box<BaseStoredDevice>('triggers').length.toString();
+    props['Number Of Sequences'] = SentryHive.box<MoveList>('sequences').length.toString();
+    props['Number Of Triggers'] = SentryHive.box<Trigger>('triggers').length.toString();
 
     // Http Post request see https://plausible.io/docs/events-api
     try {
