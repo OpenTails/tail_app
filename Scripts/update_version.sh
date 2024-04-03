d2c92792404333480e9aaa571235e31d426dafd2
Codel1417 <13484789+Codel1417@users.noreply.github.com>
2024-04-03 12:10:26 -0400

    activate package before calling

--- Scripts/update_version.sh
+++ Scripts/update_version.sh
@@ -3,4 +3,6 @@ set -e
 set -x
 VERSION=$(cat ../VERSION)
 BUILD_NUMBER=$(git rev-list HEAD --count)
-cd .. && dart pub global run pubspec_version_cli:pubspec_version change --version "$VERSION"+"$BUILD_NUMBER"
+dart pub global activate pubspec_version_cli
+cd ..
+dart pub global run pubspec_version_cli:pubspec_version change --version "$VERSION"+"$BUILD_NUMBER"
