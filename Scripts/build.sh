25545096d016893eff53b7f087668a189d98e5ac
Codel1417 <13484789+Codel1417@users.noreply.github.com>
2024-04-03 19:54:47 -0400

    shrink build and move build commands to sh

---
+++ Scripts/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+set -x
+if [[ "$(pwd)" == *"Scripts"* ]]; then
+  cd ..
+fi
+flutter config --no-cli-animations
+flutter pub get
+flutter gen-l10n
+dart run intl_translation:generate_from_arb --output-dir=lib/l10n --no-use-deferred-loading lib/Frontend/intnDefs.dart lib/l10n/*.arb
+flutter pub run build_runner build --delete-conflicting-outputs
+flutter build apk --split-per-abi --split-debug-info --build-number="$(git rev-list HEAD --count)"
+flutter build appbundle --split-debug-info=./symbols
--- Scripts/update_version.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-set -x
-VERSION=$(cat ../VERSION)
-BUILD_NUMBER=$(git rev-list HEAD --count)
-dart pub global activate pubspec_version_cli
-cd ..
-dart pub global run pubspec_version_cli:pubspec_version change --version "$VERSION"+"$BUILD_NUMBER"
