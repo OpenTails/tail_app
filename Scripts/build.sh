686c0dac39dd222ede14c66b6ac1106eda023fbd
Codel1417 <13484789+Codel1417@users.noreply.github.com>
2024-04-03 20:04:15 -0400

    pass build number to appbundle

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -10,4 +10,4 @@ flutter gen-l10n
 dart run intl_translation:generate_from_arb --output-dir=lib/l10n --no-use-deferred-loading lib/Frontend/intnDefs.dart lib/l10n/*.arb
 flutter pub run build_runner build --delete-conflicting-outputs
 flutter build apk --split-per-abi --split-debug-info=./symbols --build-number="$(git rev-list HEAD --count)"
-flutter build appbundle --split-debug-info=./symbols
+flutter build appbundle --split-debug-info=./symbols --build-number="$(git rev-list HEAD --count)"
