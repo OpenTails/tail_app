ea930b58fb0cd914d5de07ada62402732897f918
Codel1417 <13484789+Codel1417@users.noreply.github.com>
2024-04-03 20:01:23 -0400

    pass symbol path to apk

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -9,5 +9,5 @@ flutter pub get
 flutter gen-l10n
 dart run intl_translation:generate_from_arb --output-dir=lib/l10n --no-use-deferred-loading lib/Frontend/intnDefs.dart lib/l10n/*.arb
 flutter pub run build_runner build --delete-conflicting-outputs
-flutter build apk --split-per-abi --split-debug-info --build-number="$(git rev-list HEAD --count)"
+flutter build apk --split-per-abi --split-debug-info=./symbols --build-number="$(git rev-list HEAD --count)"
 flutter build appbundle --split-debug-info=./symbols
