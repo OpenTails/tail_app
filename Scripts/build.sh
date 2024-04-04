7548a276c873b5e96c3c949fe2096cb1a07cd171
Codel1417 <13484789+Codel1417@users.noreply.github.com>
2024-04-03 20:38:53 -0400

    set SENTRY_DIST in build sh

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 set -x
-if [[ "$(pwd)" == *"Scripts"* ]]; then
+if [[ "$(pwd)" == *"/Scripts" ]]; then
   cd ..
 fi
 flutter config --no-cli-animations
@@ -11,3 +11,6 @@ dart run intl_translation:generate_from_arb --output-dir=lib/l10n --no-use-defer
 flutter pub run build_runner build --delete-conflicting-outputs
 flutter build apk --split-per-abi --split-debug-info=./symbols --build-number="$(git rev-list HEAD --count)"
 flutter build appbundle --split-debug-info=./symbols --build-number="$(git rev-list HEAD --count)"
+if [[ ! -v GITHUB_ENV ]]; then
+  echo "SENTRY_DIST=$(git rev-list HEAD --count)" >> "$GITHUB_ENV"
+fi
