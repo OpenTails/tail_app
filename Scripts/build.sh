6b93fa20187eeafbec2ffae305ffb108ce83fe96
Code-Floof <13484789+Codel1417@users.noreply.github.com>
2024-05-16 23:12:45 -0400

    Update build.sh

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -39,10 +39,10 @@ if [[ ! -v SKIP_BUILD ]]; then # This is re-used for the linting job, which does
     cd ios
     pod install
     cd ..
-    flutter build ipa $DEBUG --no-codesign --build-number="$BUILD_NUMBER" --build-name="$VERSION" --dart-define=cronetHttpNoPlay=false
+    flutter build ipa $DEBUG --no-codesign --build-number="$BUILD_NUMBER" --build-name="$VERSION" 
   else
-    flutter build apk --split-debug-info=./symbols $DEBUG --build-number="$BUILD_NUMBER" --build-name="$VERSION" --dart-define=cronetHttpNoPlay=true
-    flutter build appbundle --split-debug-info=./symbols --build-number="$BUILD_NUMBER" --build-name="$VERSION" --dart-define=cronetHttpNoPlay=false
+    flutter build apk --split-debug-info=./symbols $DEBUG --build-number="$BUILD_NUMBER" --build-name="$VERSION" #--dart-define=cronetHttpNoPlay=true
+    flutter build appbundle --split-debug-info=./symbols --build-number="$BUILD_NUMBER" --build-name="$VERSION" #--dart-define=cronetHttpNoPlay=true
   fi
 
   if [[ -v GITHUB_OUTPUT ]]; then
