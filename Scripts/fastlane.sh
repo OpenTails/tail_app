97cc7950111e9a7853115004d4eff0effe3439d0
Code-Floof <13484789+Codel1417@users.noreply.github.com>
2024-05-20 17:28:38 -0400

    Update fastlane.sh

--- Scripts/fastlane.sh
+++ Scripts/fastlane.sh
@@ -15,12 +15,10 @@ fi
 
   if [[ $OS == 'macos-latest' ]]; then
     cd ios
-    bundle install
     echo "$APPLE_SECRETS" > APPLE_SECRETS.json
-    bundle exec fastlane beta
+    fastlane beta
     rm APPLE_SECRETS.json
   else
     cd android
-    bundle install
-    bundle exec fastlane beta
+    fastlane beta
   fi
