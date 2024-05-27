6ca80c281e035df4f249d11912f4c6e5f103b9b6
Codel1417 <13484789+Codel1417@users.noreply.github.com>
2024-05-27 16:08:09 -0400

    fix exception when signing config missing

--- android/wear/build.gradle.kts
+++ android/wear/build.gradle.kts
@@ -58,11 +58,13 @@ android {
         }
     }
     signingConfigs {
-        create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = file(keystoreProperties["storeFile"] as String)
-            storePassword = keystoreProperties["storePassword"] as String
+        if (keystorePropertiesFile.exists()) {
+            create("release") {
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+                storeFile = file(keystoreProperties["storeFile"] as String)
+                storePassword = keystoreProperties["storePassword"] as String
+            }
         }
     }
     buildTypes {
