ef0ceb4f8ac4a1fa29f8fbd5c77444c176acf378
Code-Floof <13484789+Codel1417@users.noreply.github.com>
2024-05-28 00:53:40 -0400

    Update build_gh.sh

--- Scripts/build_gh.sh
+++ Scripts/build_gh.sh
@@ -14,23 +14,15 @@ if [[ $GITHUB_EVENT_NAME == 'pull_request'  ]]; then
   DEBUG="--debug"
 else
   DEBUG="--release"
+  echo "$ANDROID_KEY_PROPERTIES" > ./android/key.properties
+  echo -n "$ANDROID_KEY_JKS" | base64 -d > ./android/AndroidKeystoreCodel1417.jks
 fi
 
-if [[ ! -v SKIP_BUILD ]]; then # This is re-used for the linting job, which doesn't require a full build
-  # Build
-  if [[ $RUNNER_OS == 'macOS' ]]; then
-    echo doing nothing Awoo
-    #flutter build ipa $DEBUG --no-codesign --build-number="$BUILD_NUMBER" --build-name="$VERSION"
-  else
-    if [[ $GITHUB_EVENT_NAME == 'push' ]]; then
-        echo "$ANDROID_KEY_PROPERTIES" > ./android/key.properties
-        echo -n "$ANDROID_KEY_JKS" | base64 -d > ./android/AndroidKeystoreCodel1417.jks
-    fi
-    echo "::group::Build APK"
-    flutter build apk --split-debug-info=./symbols $DEBUG --build-number="$BUILD_NUMBER" --build-name="$VERSION" --color #--dart-define=cronetHttpNoPlay=true
-    echo "::endgroup::"
-    echo "::group::Build APK"
-    flutter build appbundle --split-debug-info=./symbols --build-number="$BUILD_NUMBER" --build-name="$VERSION" --color #--dart-define=cronetHttpNoPlay=true
-    echo "::endgroup::"
-  fi
+echo "::group::Build APK"
+flutter build apk --split-debug-info=./symbols $DEBUG --build-number="$BUILD_NUMBER" --build-name="$VERSION" --color #--dart-define=cronetHttpNoPlay=true
+echo "::endgroup::"
+echo "::group::Build APK"
+flutter build appbundle --split-debug-info=./symbols --build-number="$BUILD_NUMBER" --build-name="$VERSION" --color #--dart-define=cronetHttpNoPlay=true
+echo "::endgroup::"
+
 fi
