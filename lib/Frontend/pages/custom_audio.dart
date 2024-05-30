63da2d5c6627b2b2a1f40f0f1a7305b42bcd7d7a
Codel1417 <awoo@codel1417.xyz>
2024-05-30 13:03:17 -0400

    allow tapping on audio entry on edit screen to play sound

--- lib/Frontend/pages/custom_audio.dart
+++ lib/Frontend/pages/custom_audio.dart
@@ -111,6 +111,7 @@ class _CustomAudioState extends ConsumerState<CustomAudio> {
                 )
               ],
             ),
+            onTap: () => playSound(audioAction.file),
           );
         },
       ),
