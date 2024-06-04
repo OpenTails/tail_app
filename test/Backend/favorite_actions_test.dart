261855acd3a421841f699c7698b6127073876133
Codel1417 <awoo@codel1417.xyz>
2024-06-04 18:53:55 -0400

    add Favorite actions provider tests

---
+++ test/Backend/favorite_actions_test.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart' as flTest;
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tail_app/Backend/Definitions/Action/base_action.dart';
+import 'package:tail_app/Backend/favorite_actions.dart';
+import 'package:tail_app/Backend/sensors.dart';
+
+import '../testing_utils/hive_utils.dart';
+
+void main() {
+  setUp(() async {
+    flTest.TestWidgetsFlutterBinding.ensureInitialized();
+    await setupHive();
+  });
+  tearDown(() async {
+    await deleteHive();
+  });
+  test('Create Favorite', () async {
+    final container = ProviderContainer(
+      overrides: [],
+    );
+    BaseAction? baseAction = container.read(getActionFromUUIDProvider('c53e980e-899e-4148-a13e-f57a8f9707f4'));
+    expect(baseAction != null, true);
+    await container.read(favoriteActionsProvider.notifier).add(baseAction!);
+    expect(container.read(favoriteActionsProvider).length, 1);
+    expect(container.read(favoriteActionsProvider).first.actionUUID, 'c53e980e-899e-4148-a13e-f57a8f9707f4');
+
+    // force re-open of provider
+
+    container.invalidate(favoriteActionsProvider);
+    expect(container.read(favoriteActionsProvider).length, 1);
+    expect(container.read(favoriteActionsProvider).first.actionUUID, 'c53e980e-899e-4148-a13e-f57a8f9707f4');
+
+    // remove
+    await container.read(favoriteActionsProvider.notifier).remove(baseAction);
+    expect(container.read(favoriteActionsProvider).length, 0);
+  });
+}
