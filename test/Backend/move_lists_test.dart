7e6474c95e85646b9c6d629d74e08f68162aeeb2
Codel1417 <awoo@codel1417.xyz>
2024-06-05 11:57:27 -0400

    Some move-list tests

--- test/Backend/move_lists_test.dart
+++ test/Backend/move_lists_test.dart
@@ -4,6 +4,7 @@ import 'package:riverpod/src/framework.dart';
 import 'package:tail_app/Backend/Bluetooth/bluetooth_manager.dart';
 import 'package:tail_app/Backend/Bluetooth/bluetooth_message.dart';
 import 'package:tail_app/Backend/Definitions/Device/device_definition.dart';
+import 'package:tail_app/Backend/LoggingWrappers.dart';
 import 'package:tail_app/Backend/move_lists.dart';
 
 import '../testing_utils/gear_utils.dart';
@@ -70,6 +71,35 @@ void main() {
       expect(commands.first.responseMSG, 'EARHOME END');
       expect(commands.first.device, baseStatefulDevice);
       expect(commands.first.type, type);
+    });
+
+    group('Storing Movelists', () {
+      test('Create Move List', () async {
+        MoveList moveList = MoveList(name: 'Test', deviceCategory: DeviceType.values, uuid: 'test');
+        final container = ProviderContainer(
+          overrides: [],
+        );
+        expect(container.read(moveListsProvider).isEmpty, true);
+        await container.read(moveListsProvider.notifier).add(moveList);
+        expect(container.read(moveListsProvider).length, 1);
+        expect(container.read(moveListsProvider).first, moveList);
+        expect(HiveProxy.getAll<MoveList>('sequences').length, 1);
+        //verify movelists are read again from hive
+        container.invalidate(moveListsProvider);
+        expect(container.read(moveListsProvider).length, 1);
+        expect(container.read(moveListsProvider).first, moveList);
+
+        await container.read(moveListsProvider.notifier).remove(moveList);
+        expect(container.read(moveListsProvider).isEmpty, true);
+        expect(HiveProxy.getAll<MoveList>('sequences').isEmpty, true);
+      });
+    });
+    test('Editing Move List record', () {
+      MoveList moveList = MoveList(name: "Test", deviceCategory: DeviceType.values, uuid: 'uuid');
+      expect(moveList.moves.isEmpty, true);
+      Move move = Move.move(leftServo: 50, rightServo: 100, easingType: EasingType.cubic, speed: 5);
+      moveList.moves.add(move);
+      expect(moveList.moves.length, 1);
     });
   });
 }
