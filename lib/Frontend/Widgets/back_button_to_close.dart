8a9e5fceda197296902cce6cd8311b9b0e28912e
Codel1417 <awoo@codel1417.xyz>
2024-06-19 17:48:35 -0400

    add more interceptors to double back to close app

--- lib/Frontend/Widgets/back_button_to_close.dart
+++ lib/Frontend/Widgets/back_button_to_close.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 import 'package:back_button_interceptor/back_button_interceptor.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../Backend/device_registry.dart';
@@ -38,33 +39,55 @@ class _BackButtonToCloseState extends ConsumerState<BackButtonToClose> {
       if (ref.read(getAvailableGearProvider).isNotEmpty) {
         return true;
       }
-      if (timer.isActive) {
-        return false;
-      }
-      timer = Timer(const Duration(seconds: 1), () {});
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          /// need to set following properties for best effect of awesome_snackbar_content
-          elevation: 0,
-          behavior: SnackBarBehavior.floating,
-          backgroundColor: Colors.transparent,
-          content: AwesomeSnackbarContent(
-            title: doubleBackToClose(),
-            message: '',
-
-            /// change contentType to ContentType.success, ContentType.warning or ContentType.help for variants
-            contentType: ContentType.help,
-          ),
-        ),
-      );
+      showSnackbar();
       return true;
     }
-
     return false;
   }
 
   @override
   Widget build(BuildContext context) {
-    return widget.child;
+    return WillPopScope(
+      child: BackButtonListener(
+        child: PopScope(
+          canPop: false,
+          onPopInvoked: (didPop) {
+            showSnackbar();
+          },
+          child: NavigatorPopHandler(
+            enabled: true,
+            onPop: showSnackbar,
+            child: widget.child,
+          ),
+        ),
+        onBackButtonPressed: () async {
+          showSnackbar();
+          return true;
+        },
+      ),
+      onWillPop: () => Future.value(false),
+    );
+  }
+
+  void showSnackbar() {
+    if (timer.isActive) {
+      SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+    }
+    timer = Timer(const Duration(seconds: 1), () {});
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        /// need to set following properties for best effect of awesome_snackbar_content
+        elevation: 0,
+        behavior: SnackBarBehavior.floating,
+        backgroundColor: Colors.transparent,
+        content: AwesomeSnackbarContent(
+          title: doubleBackToClose(),
+          message: '',
+
+          /// change contentType to ContentType.success, ContentType.warning or ContentType.help for variants
+          contentType: ContentType.help,
+        ),
+      ),
+    );
   }
 }
