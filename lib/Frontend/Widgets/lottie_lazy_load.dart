7a439e9790d94ffde8280202c1afa15fcb4f92c4
Codel1417 <awoo@codel1417.xyz>
2024-11-30 11:27:35 -0500

    fix build & use lottie native again
    ping ponging between lottie and lottie native

--- lib/Frontend/Widgets/lottie_lazy_load.dart
+++ lib/Frontend/Widgets/lottie_lazy_load.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
+import 'package:lottie_native/lottie_native.dart';
 
 import '../../constants.dart';
 
@@ -16,49 +16,30 @@ class LottieLazyLoad extends StatefulWidget {
 
 class _LottieLazyLoadState extends State<LottieLazyLoad> with TickerProviderStateMixin {
   bool isLoaded = false;
-  late final AnimationController _controller;
 
-  @override
-  void initState() {
-    super.initState();
-
-    _controller = AnimationController(vsync: this);
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+  void onViewCreated(LottieController lottieController) {
+    setState(() {
+      isLoaded = true;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: AnimatedOpacity(
-      opacity: _controller.isAnimating ? 1 : 0,
+      opacity: isLoaded ? 1 : 0,
       duration: animationTransitionDuration,
-      child: Lottie.asset(
+      child: SizedBox(
         width: widget.width,
-        renderCache: widget.renderCache ? RenderCache.raster : null,
-        widget.asset,
-        controller: _controller,
-        onLoaded: (p0) {
-          // Configure the AnimationController with the duration of the
-          // Lottie file and start the animation.
-          _controller
-            ..duration = p0.duration
-            ..forward()
-            ..addListener(
-              () {
-                if (_controller.isCompleted && context.mounted) {
-                  _controller.repeat();
-                }
-              },
-            );
-          setState(() {
-            isLoaded = true;
-          });
-        },
+        height: widget.width,
+        child: SafeArea(
+          child: LottieView.fromAsset(
+            filePath: widget.asset,
+            autoPlay: true,
+            loop: true,
+            onViewCreated: onViewCreated,
+          ),
+        ),
       ),
     ));
   }
