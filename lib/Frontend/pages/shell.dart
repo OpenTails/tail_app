981f6bbfbf7cf6002d7f948e629776a8f029ac97
Codel1417 <awoo@codel1417.xyz>
2025-06-08 13:37:58 -0400

    re-add app store update check
    Stop using old versions pls

--- lib/Frontend/pages/shell.dart
+++ lib/Frontend/pages/shell.dart
@@ -5,6 +5,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tail_app/Frontend/Widgets/wear_helper.dart';
+import 'package:tail_app/Frontend/go_router_config.dart';
+import 'package:upgrader/upgrader.dart';
 
 import '../Widgets/back_button_to_close.dart';
 import '../Widgets/known_gear.dart';
@@ -52,63 +54,68 @@ class _NavigationDrawerExampleState extends ConsumerState<NavigationDrawerExampl
     ref.watch(initLocaleProvider);
     return BackButtonToClose(
       child: WearHelper(
+        child: UpgradeAlert(
+          dialogStyle: UpgradeDialogStyle.material,
+          navigatorKey: rootNavigatorKey,
           child: AdaptiveScaffold(
-        // An option to override the default breakpoints used for small, medium,
-        // and large.
-        smallBreakpoint: const Breakpoint(endWidth: 700),
-        mediumBreakpoint: const Breakpoint(beginWidth: 700, endWidth: 1000),
-        largeBreakpoint: const Breakpoint(beginWidth: 1000),
-        useDrawer: false,
-        internalAnimations: false,
-        transitionDuration: Duration.zero,
-        appBarBreakpoint: const Breakpoint(beginWidth: 0),
-        selectedIndex: screenIndex,
-        onSelectedIndexChange: (int index) {
-          setState(
-            () {
-              screenIndex = index;
-              return GoRouter.of(context).go(destinations[index].path);
+            // An option to override the default breakpoints used for small, medium,
+            // and large.
+            smallBreakpoint: const Breakpoint(endWidth: 700),
+            mediumBreakpoint: const Breakpoint(beginWidth: 700, endWidth: 1000),
+            largeBreakpoint: const Breakpoint(beginWidth: 1000),
+            useDrawer: false,
+            internalAnimations: false,
+            transitionDuration: Duration.zero,
+            appBarBreakpoint: const Breakpoint(beginWidth: 0),
+            selectedIndex: screenIndex,
+            onSelectedIndexChange: (int index) {
+              setState(
+                () {
+                  screenIndex = index;
+                  return GoRouter.of(context).go(destinations[index].path);
+                },
+              );
             },
-          );
-        },
-        destinations: destinations.map(
-          (NavDestination destination) {
-            return NavigationDestination(
-              label: destination.label,
-              icon: destination.icon,
-              selectedIcon: destination.selectedIcon,
-              tooltip: destination.label,
-            );
-          },
-        ).toList(),
-        body: (_) => SafeArea(
-          bottom: false,
-          top: false,
-          child: SnackBarOverlay(
-            child: widget.child,
+            destinations: destinations.map(
+              (NavDestination destination) {
+                return NavigationDestination(
+                  label: destination.label,
+                  icon: destination.icon,
+                  selectedIcon: destination.selectedIcon,
+                  tooltip: destination.label,
+                );
+              },
+            ).toList(),
+            body: (_) => SafeArea(
+              bottom: false,
+              top: false,
+              child: SnackBarOverlay(
+                child: widget.child,
+              ),
+            ),
+            // smallBody: (_) => SafeArea(
+            //   bottom: false,
+            //   top: false,
+            //   child: SnackBarOverlay(
+            //     child: widget.child,
+            //   ),
+            // ),
+            // Define a default secondaryBody.
+            //secondaryBody: AdaptiveScaffold.emptyBuilder,
+            // Override the default secondaryBody during the smallBreakpoint to be
+            // empty. Must use AdaptiveScaffold.emptyBuilder to ensure it is properly
+            // overridden.
+            smallSecondaryBody: AdaptiveScaffold.emptyBuilder,
+            appBar: AppBar(
+              title: const DeviceStatusWidget(),
+              centerTitle: true,
+              leadingWidth: 0,
+              titleSpacing: 0,
+              toolbarHeight: 100 * MediaQuery.textScalerOf(context).scale(1),
+            ),
           ),
         ),
-        // smallBody: (_) => SafeArea(
-        //   bottom: false,
-        //   top: false,
-        //   child: SnackBarOverlay(
-        //     child: widget.child,
-        //   ),
-        // ),
-        // Define a default secondaryBody.
-        //secondaryBody: AdaptiveScaffold.emptyBuilder,
-        // Override the default secondaryBody during the smallBreakpoint to be
-        // empty. Must use AdaptiveScaffold.emptyBuilder to ensure it is properly
-        // overridden.
-        smallSecondaryBody: AdaptiveScaffold.emptyBuilder,
-        appBar: AppBar(
-          title: const DeviceStatusWidget(),
-          centerTitle: true,
-          leadingWidth: 0,
-          titleSpacing: 0,
-          toolbarHeight: 100 * MediaQuery.textScalerOf(context).scale(1),
-        ),
-      )),
+      ),
     );
   }
 }
