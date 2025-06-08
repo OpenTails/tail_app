8302d1eec512193da5e61b4f0cfcab21971f81aa
Codel1417 <awoo@codel1417.xyz>
2025-06-08 18:50:47 -0400

    UwU is now 200% more OwO

--- lib/Frontend/Widgets/uwu_text.dart
+++ lib/Frontend/Widgets/uwu_text.dart
@@ -1,9 +1,10 @@
 import 'package:tail_app/Backend/logging_wrappers.dart';
 import 'package:tail_app/constants.dart';
+import 'package:owoify_dart/owoify_dart.dart';
 
 String convertToUwU(String input) {
   if (HiveProxy.getOrDefault(settings, uwuTextEnabled, defaultValue: uwuTextEnabledDefault)) {
-    return input.replaceAll("r", "w").replaceAll("R", "W").replaceAll("l", "w").replaceAll("L", "W");
+    return Owoifier.owoify(input, level: OwoifyLevel.Uvu);
   }
   return input;
 }
