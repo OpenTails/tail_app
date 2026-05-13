d5320287131ce2e7eafa94aa0b92713abcd1ac3f
Code-Floof <awoo@codel1417.xyz>
2026-05-13 03:28:56 -0400

    migrate build.gradle to kotlin

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,79 @@
+import java.io.FileInputStream
+import java.util.Properties
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystorePropertiesFile = rootProject.file("key.properties")
+val keystoreProperties = Properties()
+keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+kotlin {
+    compilerOptions {
+        languageVersion =
+            org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0
+        // Optional: Set jvmTarget
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+    }
+}
+android {
+    namespace = "com.codel1417.tailApp"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+    defaultConfig {
+        applicationId = "com.codel1417.tailApp"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+        ndk {
+            // Filter for architectures supported by Flutter
+            abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86_64")
+        }
+    }
+    buildTypes {
+        getByName("release") {
+            // Deactivate R8.
+            isMinifyEnabled = false
+            isShrinkResources = false
+            ndk {
+                debugSymbolLevel = "FULL"
+            }
+            signingConfig = signingConfigs.getByName("release")
+        }
+        getByName("debug") {
+            isDebuggable = true
+            signingConfig = if (file(keystorePropertiesFile).exists())
+                signingConfigs
+                    .getByName("release") else signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.3.20")
+    testImplementation("junit:junit:4.13.2")
+
+}
---
+++ android/build.gradle.kts
@@ -0,0 +1,75 @@
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+var flutterCompileSdkVersion = 36
+subprojects {
+    afterEvaluate {
+        plugins.withType<com.flutter.gradle.FlutterPlugin> {
+            configure<com.flutter.gradle.FlutterExtension> {
+                flutterCompileSdkVersion = compileSdkVersion
+            }
+        }
+
+        plugins.withType<com.android.build.gradle.BasePlugin> {
+            configure<com.android.build.gradle.BaseExtension> {
+                val current =
+                    (compileSdkVersion ?: "android-1").substring(8).toInt()
+                if (current < flutterCompileSdkVersion) {
+                    compileSdkVersion(flutterCompileSdkVersion)
+                }
+
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_17
+                    targetCompatibility = JavaVersion.VERSION_17
+                }
+            }
+        }
+
+        plugins.withType<org.jetbrains.kotlin.gradle.plugin.KotlinBasePlugin> {
+            configure<org.jetbrains.kotlin.gradle.dsl.KotlinBaseExtension> {
+                jvmToolchain(17)
+            }
+        }
+
+        plugins.withType<org.gradle.api.plugins.JavaBasePlugin> {
+            configure<org.gradle.api.plugins.JavaPluginExtension> {
+                toolchain {
+                    languageVersion = JavaLanguageVersion.of(17)
+                }
+            }
+        }
+
+        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask<org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompilerOptions>>()
+            .configureEach {
+                compilerOptions {
+                    jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+                }
+            }
+    }
+}
+
+configurations.all {
+    resolutionStrategy {
+        force("androidx.core:core-ktx:1.18.0")
+    }
+}
+val newBuildDir: Directory =
+    rootProject.layout.buildDirectory
+        .dir("../../build")
+        .get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
