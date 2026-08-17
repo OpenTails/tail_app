a2707ff0736ad87fd750ef03820aede6f8c28fff
Codel1417 <awoo@codel1417.xyz>
2026-08-17 03:47:20 -0400

    add a test flag to disable lottie animations

--- lib/Frontend/Widgets/lottie_lazy_load.dart
+++ lib/Frontend/Widgets/lottie_lazy_load.dart
@@ -1,10 +1,17 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
 import '../../constants.dart';
 
 class LottieLazyLoad extends StatefulWidget {
-  const LottieLazyLoad({super.key, required this.asset, this.renderCache = true, required this.width});
+  const LottieLazyLoad({
+    super.key,
+    required this.asset,
+    this.renderCache = true,
+    required this.width,
+  });
 
   final String asset;
   final bool renderCache;
@@ -14,7 +21,16 @@ class LottieLazyLoad extends StatefulWidget {
   State<LottieLazyLoad> createState() => _LottieLazyLoadState();
 }
 
-class _LottieLazyLoadState extends State<LottieLazyLoad> with TickerProviderStateMixin {
+bool _enableAnimation = true;
+
+/// Call before widget tests to disable lottie animations.
+@visibleForTesting
+void enableAnimations(bool enabled) {
+  _enableAnimation = enabled;
+}
+
+class _LottieLazyLoadState extends State<LottieLazyLoad>
+    with TickerProviderStateMixin {
   bool isLoaded = false;
   late final AnimationController _controller;
 
@@ -34,32 +50,35 @@ class _LottieLazyLoadState extends State<LottieLazyLoad> with TickerProviderStat
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: AnimatedOpacity(
-      opacity: _controller.isAnimating ? 1 : 0,
-      duration: animationTransitionDuration,
-      child: Lottie.asset(
-        width: widget.width,
-        renderCache: widget.renderCache ? RenderCache.raster : null,
-        widget.asset,
-        controller: _controller,
-        onLoaded: (p0) {
-          // Configure the AnimationController with the duration of the
-          // Lottie file and start the animation.
-          _controller
-            ..duration = p0.duration
-            ..forward()
-            ..addListener(
-              () {
-                if (_controller.isCompleted && context.mounted) {
-                  _controller.repeat();
-                }
-              },
-            );
-          setState(() {
-            isLoaded = true;
-          });
-        },
+      child: AnimatedOpacity(
+        opacity: _controller.isAnimating ? 1 : 0,
+        duration: animationTransitionDuration,
+        child: Lottie.asset(
+          width: widget.width,
+          renderCache: widget.renderCache ? RenderCache.raster : null,
+          widget.asset,
+          controller: _controller,
+          onLoaded: (p0) {
+            // Configure the AnimationController with the duration of the
+            // Lottie file and start the animation.
+            if (_enableAnimation) {
+              _controller
+                ..duration = p0.duration
+                ..forward()
+                ..addListener(() {
+                  if (_controller.isCompleted && context.mounted) {
+                    _controller.repeat();
+                  }
+                });
+            } else {
+              _controller.reset();
+            }
+            setState(() {
+              isLoaded = true;
+            });
+          },
+        ),
       ),
-    ));
+    );
   }
 }
